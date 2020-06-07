2241e8db53522bb09416c6de5da894f0d2774455
Salmanovich Kristina <krisisei222@gmail.com>
2020-06-07 19:29:46 +0300

    Updates

---
+++ precache-manifest.d7f763c2ccd1f31843dfca7789929060.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fbcbf2d960ce008a628aa314cf82348",
+    "url": "/react-soc-home/index.html"
+  },
+  {
+    "revision": "e635564e1f2f6756a242",
+    "url": "/react-soc-home/static/css/3.edd834f5.chunk.css"
+  },
+  {
+    "revision": "806a4effa217ba52de89",
+    "url": "/react-soc-home/static/css/4.4f2d56df.chunk.css"
+  },
+  {
+    "revision": "ae0b9915ef7c6ce9f305",
+    "url": "/react-soc-home/static/css/main.e2dc1ab0.chunk.css"
+  },
+  {
+    "revision": "6ec89d94f0b1e4d3bd1d",
+    "url": "/react-soc-home/static/js/2.e1ca599b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-soc-home/static/js/2.e1ca599b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e635564e1f2f6756a242",
+    "url": "/react-soc-home/static/js/3.55641016.chunk.js"
+  },
+  {
+    "revision": "806a4effa217ba52de89",
+    "url": "/react-soc-home/static/js/4.d36756c4.chunk.js"
+  },
+  {
+    "revision": "ae0b9915ef7c6ce9f305",
+    "url": "/react-soc-home/static/js/main.bd15af0a.chunk.js"
+  },
+  {
+    "revision": "b036b9a4cdbd2effd09e",
+    "url": "/react-soc-home/static/js/runtime-main.f03a068f.js"
+  },
+  {
+    "revision": "09cfe56c45d78c5188fe3243d325a6b0",
+    "url": "/react-soc-home/static/media/logo.09cfe56c.png"
+  },
+  {
+    "revision": "b80dd36d2816e175a8337eba2bfc2b77",
+    "url": "/react-soc-home/static/media/music.b80dd36d.png"
+  },
+  {
+    "revision": "46280a98c7ec2afd45375c2f85640877",
+    "url": "/react-soc-home/static/media/preloader.svg.46280a98.gif"
+  },
+  {
+    "revision": "7e719be79d55353a3ce6551d704e43ca",
+    "url": "/react-soc-home/static/media/user.7e719be7.png"
+  }
+]);
